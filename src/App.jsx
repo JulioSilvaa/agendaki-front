71461fb2b0b99809f146b446734a1b0e0cc30951
JulioSilvaa <juliocesar.amancio@yahoo.com.br>
2024-10-24 14:08:46 -0300

    primeiro commit com typescript setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>APP RODANDO</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
+});
